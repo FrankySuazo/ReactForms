fc70f8962522591b30dee1c3aeb56a07e41e34a3
FrankySuazo <frankysuazo@outlook.com>
2024-02-12 20:02:58 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import Authenticate from "./Authenticate.jsx";
 import SignUpForm from "./SignUpForm.jsx";
 
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,4 +1,4 @@
- import React from "react"
+import React from "react";
 import useState from "react";
 
 //Variables:
@@ -13,13 +13,13 @@ console.log(result);
 setToken(result.token);
 
 async function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    try {
-    } catch (error) {
-      setError(error.message);
-    }
-  
+  try {
+  } catch (error) {
+    setError(error.message);
+  }
+
   return (
     <>
       <h2>Sign Up</h2>
@@ -48,34 +48,34 @@ async function handleSubmit(event) {
 }
 
 export default function Authenticate({ token }) {
-    const [successMessage, setSuccessMessage] = useState(null);
-    const [error, setError] = useState(null);
-  
-    async function handleClick() {
-      try {
-        const response = await fetch(
-          "https://fsa-jwt-practice.herokuapp.com/signup",
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        const result = await response.json();
-        setSuccessMessage(result.message);
-      } catch (error) {
-        setError(error.message);
-      }
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      setSuccessMessage(result.message);
+    } catch (error) {
+      setError(error.message);
     }
-  
-    return (
-      <div>
-        <h2>Authenticate</h2>
-        {successMessage && <p>{successMessage}</p>}
-        {error && <p>{error}</p>}
-        <button onClick={handleClick}>Authenticate Token!</button>
-      </div>
-    );
   }
+
+  return (
+    <div>
+      <h2>Authenticate</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token!</button>
+    </div>
+  );
+}
